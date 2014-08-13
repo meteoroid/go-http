1479989abbd44ffce51dad6493a4384845e3520a
meteoroid <meteoroid@zoho.com>
2014-08-12 23:16:02 -0500

    Add timestamp to url string

--- http.go
+++ http.go
@@ -8,13 +8,11 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL: %s", r.Host+r.URL.Path)
+	t := time.Now().Format("2006-1-2 3:04:05")
+	fmt.Fprintf(w, "%s   %s", t, r.Host+r.URL.Path)
 }
 
 func main() {
-	t := time.Now().Format("2006-1-2 3:04:05")
-	fmt.Println(t)
-
 	http.HandleFunc("/", handler)
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
