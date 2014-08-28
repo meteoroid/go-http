f95960b2c30bf97e8a757ca37e7c837ca0a9f9fb
meteoroid <meteoroid@zoho.com>
2014-08-27 22:03:30 -0500

    Move main function

--- golog.go
+++ golog.go
@@ -8,17 +8,6 @@ import (
 	"os"
 )
 
-func logURL(w http.ResponseWriter, r *http.Request) {
-	// write URL to log file
-	log.Println(r.Host + r.URL.Path)
-	// write log file to response
-	urls, err := ioutil.ReadFile("urls.log")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Fprintf(w, "%s", string(urls))
-}
-
 func main() {
 	// configure logger
 	f, err := os.OpenFile("urls.log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0600)
@@ -33,4 +22,15 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func logURL(w http.ResponseWriter, r *http.Request) {
+	// write URL to log file
+	log.Println(r.Host + r.URL.Path)
+	// write log file to response
+	urls, err := ioutil.ReadFile("urls.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "%s", string(urls))
 }
